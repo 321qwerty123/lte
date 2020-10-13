8c601d6a343ce7a628853a975205c149e7d3506e
liuzhihang <liuzhihang@ysten.com>
2020-10-13 18:15:26 +0800

    sa-token配置

--- src/main/java/com/example/lte/LteApplication.java
+++ src/main/java/com/example/lte/LteApplication.java
@@ -1,13 +1,16 @@
 package com.example.lte;
 
+import cn.dev33.satoken.SaTokenManager;
+import cn.dev33.satoken.spring.SaTokenSetup;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@SaTokenSetup
 @SpringBootApplication
 public class LteApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LteApplication.class, args);
+        System.out.println("启动成功：sa-token配置如下：" + SaTokenManager.getConfig());
     }
 
 }
