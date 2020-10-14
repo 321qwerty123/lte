176ce48b8d6ded3d21e57f275c9feb48a4a432e6
liuzhihang <liuzhihang@ysten.com>
2020-10-14 18:35:24 +0800

    auth

--- lte-front/src/App.vue
+++ lte-front/src/App.vue
@@ -5,9 +5,7 @@
 </template>
 
 <style>
-#app {
 
-}
 
 
 </style>
---
+++ lte-front/src/views/Auth.vue
@@ -0,0 +1,33 @@
+<template>
+
+  <div class="content">
+    111
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "Auth"
+}
+</script>
+
+<style scoped>
+html,body {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+
+.content {
+  width: 500px;
+  height: 300px;
+  background: orange;
+  margin: 0 auto;
+  top: 100px;
+  position: relative;
+}
+
+</style>
