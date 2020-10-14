f80a8854f74aea6c185f6884f872b11f0731ddd6
liuzh <wy126lzh@126.com>
2020-10-14 20:10:57 +0800

    antdv

--- lte-front/src/views/Auth.vue
+++ lte-front/src/views/Auth.vue
@@ -1,14 +1,47 @@
 <template>
 
   <div class="content">
-    111
+    <a-form :form="form" :label-col="{ span: 5 }" :wrapper-col="{ span: 12 }" @submit="handleSubmit">
+      <a-form-item label="Account">
+        <a-input
+                v-decorator="['account', { rules: [{ required: true, message: 'Please input your account!' }] }]"
+        />
+      </a-form-item>
+      <a-form-item label="Pwd">
+        <a-input
+                v-decorator="['pwd', { rules: [{ required: true, message: 'Please input your pwd!' }] }]"
+        />
+      </a-form-item>
+      <a-form-item :wrapper-col="{ span: 12, offset: 5 }">
+        <a-button type="primary" html-type="submit">
+          Submit
+        </a-button>
+      </a-form-item>
+    </a-form>
   </div>
 
 </template>
 
 <script>
 export default {
-  name: "Auth"
+  name: "Auth",
+  data() {
+    return {
+      formLayout: 'horizontal',
+      form: this.$form.createForm(this, { name: 'coordinated' }),
+    };
+  },
+  methods: {
+    handleSubmit(e) {
+      e.preventDefault();
+      this.form.validateFields((err, values) => {
+        if (!err) {
+          console.log('Received values of form: ', values);
+        }
+      });
+    },
+
+  },
 }
 </script>
 
